4f4ec8468dd1bc3bb77a3a8fbd25e3392affa9b8
ByeonGooHoon <dksekfldks65@naver.com>
2017-01-20 00:10:23 +0900

    기본UI제작 및 롣이화면 제작

---
+++ app/src/main/java/com/example/kobot/food_map/MapsActivity.java
@@ -0,0 +1,201 @@
+package com.example.kobot.food_map;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    ListView listview ;
+    ListViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        // 화면을 portrait 세로화면으로 고정
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        // Adapter 생성
+        adapter = new ListViewAdapter() ;
+
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) findViewById(R.id.foodlist);
+        listview.setAdapter(adapter);
+
+        //Tab 메뉴바 생성
+        TabHost tabHost=(TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec spec1=tabHost.newTabSpec("Tab 1");
+        spec1.setContent(R.id.tab1);
+        spec1.setIndicator("맛집 지도");
+
+        TabHost.TabSpec spec2=tabHost.newTabSpec("Tab 2");
+        spec2.setIndicator("스토리");
+        spec2.setContent(R.id.tab2);
+
+        TabHost.TabSpec spec3=tabHost.newTabSpec("Tab 3");
+        spec3.setIndicator("리스트");
+        spec3.setContent(R.id.tab3);
+
+        TabHost.TabSpec spec4=tabHost.newTabSpec("Tab 4");
+        spec4.setIndicator("맛집 추가");
+        spec4.setContent(R.id.tab4);
+
+        tabHost.addTab(spec1);
+        tabHost.addTab(spec2);
+        tabHost.addTab(spec3);
+        tabHost.addTab(spec4);
+
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Box", "Account Box Black 36dp") ;
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Circle", "Account Circle Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Ind", "Assignment Ind Black 36dp") ;
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Box", "Account Box Black 36dp") ;
+
+
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Circle", "Account Circle Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Ind", "Assignment Ind Black 36dp") ;
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Box", "Account Box Black 36dp") ;
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Circle", "Account Circle Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Ind", "Assignment Ind Black 36dp") ;
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Box", "Account Box Black 36dp") ;
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Circle", "Account Circle Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Ind", "Assignment Ind Black 36dp") ;
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Box", "Account Box Black 36dp") ;
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Circle", "Account Circle Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_launcher),
+                "Ind", "Assignment Ind Black 36dp") ;
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LocationManager manager = (LocationManager)getSystemService(Context.LOCATION_SERVICE) ;
+        GPSListener gpsListener = new GPSListener();
+        long minTime = 10000;
+        float minDistance = 0;
+
+
+        manager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                minTime,
+                minDistance,
+                gpsListener);
+        manager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                minTime,
+                minDistance,
+                gpsListener);
+        Toast.makeText(getApplicationContext(), "위치 확인 시작함. 로그를 확인하세요.", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private class GPSListener implements LocationListener {
+        Double latitude;
+        Double longitude;
+        Location location;
+
+        public void onLocationChanged(Location location) {
+            //capture location data sent by current provider
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+
+            showCurrentLocation(latitude,longitude);
+
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    }
+
+    private void showCurrentLocation(final Double latitude, final Double longitude) {
+        // 현재 위치를 이용해 LatLon 객체 생성
+        LatLng curPoint = new LatLng(latitude, longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(curPoint));
+
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        MarkerOptions optFirst = new MarkerOptions();
+        optFirst.position(curPoint);// 위도 • 경도
+        optFirst.title("Current Position");// 제목 미리보기
+        optFirst.snippet("Snippet");
+        optFirst.icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+        mMap.addMarker(optFirst).showInfoWindow();
+
+    }
+}
---
+++ app/src/main/java/com/example/kobot/food_map/StartActivity.java
@@ -0,0 +1,87 @@
+package com.example.kobot.food_map;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.os.Handler;
+
+public class StartActivity extends AppCompatActivity {
+
+    ImageView imgview1;
+    private AnimationDrawable mAnimationDrawable_2;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        // 화면을 portrait 세로화면으로 고정
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_start);
+        imgview1 = (ImageView) findViewById(R.id.imageView);
+
+        BitmapDrawable mBitmapDrawable_1 = (BitmapDrawable)getResources().getDrawable(R.drawable.rice1);
+        BitmapDrawable mBitmapDrawable_2 = (BitmapDrawable)getResources().getDrawable(R.drawable.rice2);
+        BitmapDrawable mBitmapDrawable_3 = (BitmapDrawable)getResources().getDrawable(R.drawable.rice3);
+        BitmapDrawable mBitmapDrawable_4 = (BitmapDrawable)getResources().getDrawable(R.drawable.rice4);
+        BitmapDrawable mBitmapDrawable_5 = (BitmapDrawable)getResources().getDrawable(R.drawable.rice5);
+        BitmapDrawable mBitmapDrawable_6 = (BitmapDrawable)getResources().getDrawable(R.drawable.rice6);
+        BitmapDrawable mBitmapDrawable_7 = (BitmapDrawable)getResources().getDrawable(R.drawable.rice7);
+        BitmapDrawable mBitmapDrawable_8 = (BitmapDrawable)getResources().getDrawable(R.drawable.rice8);
+        BitmapDrawable mBitmapDrawable_9 = (BitmapDrawable)getResources().getDrawable(R.drawable.rice9);
+        BitmapDrawable mBitmapDrawable_10 = (BitmapDrawable)getResources().getDrawable(R.drawable.rice10);
+        BitmapDrawable mBitmapDrawable_11 = (BitmapDrawable)getResources().getDrawable(R.drawable.rice11);
+        BitmapDrawable mBitmapDrawable_12 = (BitmapDrawable)getResources().getDrawable(R.drawable.rice12);
+        BitmapDrawable mBitmapDrawable_13 = (BitmapDrawable)getResources().getDrawable(R.drawable.rice13);
+
+        mAnimationDrawable_2 = new AnimationDrawable();
+        mAnimationDrawable_2.setOneShot(false);
+
+        int duration = 140;
+
+        mAnimationDrawable_2.addFrame(mBitmapDrawable_1, duration);
+        mAnimationDrawable_2.addFrame(mBitmapDrawable_2, duration);
+        mAnimationDrawable_2.addFrame(mBitmapDrawable_3, duration);
+        mAnimationDrawable_2.addFrame(mBitmapDrawable_4, duration);
+        mAnimationDrawable_2.addFrame(mBitmapDrawable_5, duration);
+        mAnimationDrawable_2.addFrame(mBitmapDrawable_6, duration);
+        mAnimationDrawable_2.addFrame(mBitmapDrawable_7, duration);
+        mAnimationDrawable_2.addFrame(mBitmapDrawable_8, duration);
+        mAnimationDrawable_2.addFrame(mBitmapDrawable_9, duration);
+        mAnimationDrawable_2.addFrame(mBitmapDrawable_10, duration);
+        mAnimationDrawable_2.addFrame(mBitmapDrawable_11, duration);
+        mAnimationDrawable_2.addFrame(mBitmapDrawable_12, duration);
+        mAnimationDrawable_2.addFrame(mBitmapDrawable_13, duration);
+
+        imgview1.setBackgroundDrawable(mAnimationDrawable_2);
+        Handler hd = new Handler();
+        hd.postDelayed(new splashhandler() , 5000); // 5초 후에 hd Handler 실행
+
+    }
+
+    private class splashhandler implements Runnable{
+        public void run() {
+            startActivity(new Intent(getApplication(), MapsActivity.class)); // 로딩이 끝난후 이동할 Activity
+            StartActivity.this.finish(); // 로딩페이지 Activity Stack에서 제거
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            // 어플에 포커스가 갈때 시작된다
+            mAnimationDrawable_2.start();
+        } else {
+            // 어플에 포커스를 떠나면 종료한다
+            mAnimationDrawable_2.stop();
+        }
+    }
+}
